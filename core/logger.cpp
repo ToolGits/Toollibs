50eacc190f541e84403d91b079949a12f8152882
GitBob <enzobobdevvideos04@gmail.com>
2026-06-02 15:31:27 -0300

    Atualizar o logger.cpp

--- core/logger.cpp
+++ core/logger.cpp
@@ -3,67 +3,76 @@
 
 #include <iostream>
 #include <ctime>
+#include <iomanip>
+#include <sstream>
 
 namespace Toollibs {
 
-    static std::string GetTime()
-    {
-        time_t now = time(nullptr);
-        tm* localTime = localtime(&now);
+// =========================
+// SAFE TIME FORMATTER
+// =========================
+static std::string GetTime()
+{
+    std::time_t now = std::time(nullptr);
 
-        char buffer[9];
-        strftime(buffer, sizeof(buffer), "%H:%M:%S", localTime);
+    std::tm localTime{};
 
-        return buffer;
-    }
+#if defined(_WIN32)
+    localtime_s(&localTime, &now);
+#else
+    localtime_r(&now, &localTime);
+#endif
 
-    void Logger::Log(LogLevel level, const std::string& message)
-    {
-        std::string levelText;
+    std::ostringstream oss;
+    oss << std::put_time(&localTime, "%H:%M:%S");
 
-        switch (level) {
-            case LogLevel::Info:
-                levelText = "INFO";
-                break;
-
-            case LogLevel::Warning:
-                levelText = "WARNING";
-                break;
-
-            case LogLevel::Error:
-                levelText = "ERROR";
-                break;
-
-            case LogLevel::Debug:
-                levelText = "DEBUG";
-                break;
-        }
-
-        std::cout
-            << "[" << GetTime() << "] "
-            << "[" << levelText << "] "
-            << message
-            << std::endl;
-    }
-
-    void Logger::Info(const std::string& message)
-    {
-        Log(LogLevel::Info, message);
-    }
-
-    void Logger::Warning(const std::string& message)
-    {
-        Log(LogLevel::Warning, message);
-    }
-
-    void Logger::Error(const std::string& message)
-    {
-        Log(LogLevel::Error, message);
-    }
-
-    void Logger::Debug(const std::string& message)
-    {
-        Log(LogLevel::Debug, message);
-    }
-
+    return oss.str();
 }
+
+// =========================
+// MAIN LOGGER
+// =========================
+void Logger::Log(LogLevel level, const std::string& message)
+{
+    std::string levelText = "UNKNOWN";
+
+    switch (level)
+    {
+        case LogLevel::Info:    levelText = "INFO";    break;
+        case LogLevel::Warning: levelText = "WARNING"; break;
+        case LogLevel::Error:   levelText = "ERROR";   break;
+        case LogLevel::Debug:   levelText = "DEBUG";   break;
+        default: break;
+    }
+
+    std::cout
+        << "[" << GetTime() << "] "
+        << "[" << levelText << "] "
+        << message
+        << std::endl;
+}
+
+// =========================
+// WRAPPERS
+// =========================
+void Logger::Info(const std::string& message)
+{
+    Log(LogLevel::Info, message);
+}
+
+void Logger::Warning(const std::string& message)
+{
+    Log(LogLevel::Warning, message);
+}
+
+void Logger::Error(const std::string& message)
+{
+    Log(LogLevel::Error, message);
+}
+
+void Logger::Debug(const std::string& message)
+{
+    Log(LogLevel::Debug, message);
+}
+
+} // namespace Toollibs
