14c1d83a266679d3bbfd88755d41adb544fd961c
GitBob <enzobobdevvideos04@gmail.com>
2026-06-06 11:23:04 -0300

    Atualizar o plugin.hpp

--- plugins/POP/plugin.hpp
+++ plugins/POP/plugin.hpp
@@ -1,10 +1,12 @@
+
 #ifndef TOOLLIBS_POP_PLUGIN_HPP
 #define TOOLLIBS_POP_PLUGIN_HPP
 
 namespace POP
 {
     void Initialize();
-    void Run();
+    void GenerateGitKeeps(const char* rootPath);
+    void GitAdd();
 }
 
 #endif
